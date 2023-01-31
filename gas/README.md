927f86f3d12a5e2f85498a2d5a83174690ed0918
QBurst <anantha.balaji@qburst.com>
2023-01-31 17:44:13 +0530

    Gas estimations

---
+++ gas/README.md
@@ -0,0 +1,6 @@
+Gas Estimation parameters:
+--------------------------
+    - Gas units
+    - Gas Price(gwei)
+    - Tx Fee = Gas units + (base fee * Priority Fee(tip))
+    - Refund = Tx Fee Sent(Gas Limit) - Tx cost
---
+++ gas/gas_fee_estimation.sol
@@ -0,0 +1,22 @@
+pragma solidity >0.7.0;
+
+contract LargestHolder {
+    function submitBalances(
+        uint256[] memory _balances,
+        address[] memory _holders
+    ) public {
+        // Write your code here
+    }
+
+    function process() public {
+        // Write your code here
+    }
+
+    function numberOfTxRequired() public view returns (uint256) {
+        // Write your code here
+    }
+
+    function getLargestHolder() public view returns (address) {
+        // Write your code here
+    }
+}
